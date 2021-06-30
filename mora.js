2a056daad5fd03d12ab6c8df57a86afed04521fe
PingHao0203 <77473327+PingHao0203@users.noreply.github.com>
2021-06-30 11:46:53 +0800

    Update mora.js

--- mora.js
+++ mora.js
@@ -11,12 +11,20 @@ document.querySelector(".button_rock").addEventListener("click", function () {
   if (computer_mora == 1) {
     img1.src = "C:/Users/djgoc/Desktop/web/img/rock.jpg";
     document.querySelector("#player-result").textContent = "Tie😒";
+    document.querySelector(".player-box").style.background =
+      "rgba(172, 59, 255, 0.85)";
+    document.querySelector(".computer-box").style.background =
+      "rgba(172, 59, 255, 0.55)";
   } else if (computer_mora == 2) {
     img1.src = "C:/Users/djgoc/Desktop/web/img/paper.jpg";
     document.querySelector("#player-result").textContent = "Lose🙄";
+    document.querySelector(".player-box").style.background = "#E00000";
+    document.querySelector(".computer-box").style.background = "#009900";
   } else if (computer_mora == 3) {
     document.querySelector("#player-result").textContent = "Win😁";
     img1.src = "C:/Users/djgoc/Desktop/web/img/scissors.jpg";
+    document.querySelector(".player-box").style.background = "#009900";
+    document.querySelector(".computer-box").style.background = "#E00000";
   }
 });
 
@@ -25,12 +33,20 @@ document.querySelector(".button_paper").addEventListener("click", function () {
   if (computer_mora == 1) {
     img1.src = "C:/Users/djgoc/Desktop/web/img/rock.jpg";
     document.querySelector("#player-result").textContent = "Tie😒";
+    document.querySelector(".player-box").style.background =
+      "rgba(172, 59, 255, 0.85)";
+    document.querySelector(".computer-box").style.background =
+      "rgba(172, 59, 255, 0.55)";
   } else if (computer_mora == 2) {
     img1.src = "C:/Users/djgoc/Desktop/web/img/paper.jpg";
     document.querySelector("#player-result").textContent = "Lose🙄";
+    document.querySelector(".player-box").style.background = "#E00000";
+    document.querySelector(".computer-box").style.background = "#009900";
   } else if (computer_mora == 3) {
-    img1.src = "C:/Users/djgoc/Desktop/web/img/scissors.jpg";
     document.querySelector("#player-result").textContent = "Win😁";
+    img1.src = "C:/Users/djgoc/Desktop/web/img/scissors.jpg";
+    document.querySelector(".player-box").style.background = "#009900";
+    document.querySelector(".computer-box").style.background = "#E00000";
   }
 });
 
@@ -41,11 +57,19 @@ document
     if (computer_mora == 1) {
       img1.src = "C:/Users/djgoc/Desktop/web/img/rock.jpg";
       document.querySelector("#player-result").textContent = "Tie😒";
+      document.querySelector(".player-box").style.background =
+        "rgba(172, 59, 255, 0.85)";
+      document.querySelector(".computer-box").style.background =
+        "rgba(172, 59, 255, 0.55)";
     } else if (computer_mora == 2) {
       img1.src = "C:/Users/djgoc/Desktop/web/img/paper.jpg";
       document.querySelector("#player-result").textContent = "Lose🙄";
+      document.querySelector(".player-box").style.background = "#E00000";
+      document.querySelector(".computer-box").style.background = "#009900";
     } else if (computer_mora == 3) {
-      img1.src = "C:/Users/djgoc/Desktop/web/img/scissors.jpg";
       document.querySelector("#player-result").textContent = "Win😁";
+      img1.src = "C:/Users/djgoc/Desktop/web/img/scissors.jpg";
+      document.querySelector(".player-box").style.background = "#009900";
+      document.querySelector(".computer-box").style.background = "#E00000";
     }
   });
